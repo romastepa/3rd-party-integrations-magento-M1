27cb48e226b396a59cd77a40d739d7e2034741d6
Roman Stepanov <mail2punk@gmail.com>
2019-02-28 06:59:17 +0200

    customers/subscribers duplicates fix

--- app/code/community/Emarsys/Suite2/Model/Api/Payload/Subscriber/Item/Collection.php
+++ app/code/community/Emarsys/Suite2/Model/Api/Payload/Subscriber/Item/Collection.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
  * API Customer Item collection
- * 
+ *
  * Created to generate correct toArray based on mapping
  */
 class Emarsys_Suite2_Model_Api_Payload_Subscriber_Item_Collection extends Emarsys_Suite2_Model_Api_Payload_Customer_Item_Collection
 {
-    protected $_itemFactoryName = 'emarsys_suite2/api_payload_subscriber_item';    
-    
+    protected $_itemFactoryName = 'emarsys_suite2/api_payload_subscriber_item';
+
     /**
      * Returns key identifier
-     * 
+     *
      * @return string
      */
     protected function _getKeyId()
@@ -25,16 +26,16 @@ class Emarsys_Suite2_Model_Api_Payload_Subscriber_Item_Collection extends Emarsy
      */
     public function getPayload()
     {
-        $arrItems = array();
+        $arrItems = [];
         $arrItems['key_id'] = $this->_getKeyId();
-        $arrItems['contacts'] = array();
+        $arrItems['contacts'] = [];
         $isNewCustomerSubscriber = false;
         foreach ($this as $item) {
             if (!$item->isSubscriberExists()) {
                 $arrItems['contacts'][] = $item->toArray();
             }
 
-            if($item->getDataObject()->isObjectNew() && $item->getDataObject()->getCustomerId()) {
+            if ($item->getDataObject()->isObjectNew() && $item->getDataObject()->getCustomerId()) {
                 $isNewCustomerSubscriber = true;
             } else {
                 $isNewCustomerSubscriber = false;
@@ -45,7 +46,7 @@ class Emarsys_Suite2_Model_Api_Payload_Subscriber_Item_Collection extends Emarsy
             return null;
         }
         /* Use the KEY ID as customer ID for new registration with subscription to avoid creating two contact records in Emarsys */
-        if($isNewCustomerSubscriber){
+        if ($isNewCustomerSubscriber) {
             $arrItems['key_id'] = $this->_getConfig()->getEmarsysCustomerKeyId();
         }
 
